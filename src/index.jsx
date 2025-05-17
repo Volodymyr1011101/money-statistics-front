49a4d9681d0ebe562c8d7805381aa44171bae6c3
Valeriia Martynova <vmartynova95@gmail.com>
2025-05-17 22:29:37 +0300

    add favicon, name app, font, global styles

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {Provider} from "react-redux"
-import {store} from "./redux/store"
-import {RouterProvider} from "react-router-dom";
-import {router} from "./router/router";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "modern-normalize";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <Provider store={store} >
-          <RouterProvider router={router} >
-              <App />
-          </RouterProvider>
-      </Provider>
+    <Provider store={store}>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </Provider>
   </React.StrictMode>
 );
 
