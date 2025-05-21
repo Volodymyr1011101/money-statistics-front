2c0fbf41e0797ea9e5a770b9af49088dc23ac297
EugeneyCh <eugeney@ua.fm>
2025-05-21 16:14:21 +0300

    fixTransactionList

--- src/components/UserAcountLayout/TransactionsList/TransactionsList.jsx
+++ src/components/UserAcountLayout/TransactionsList/TransactionsList.jsx
@@ -4,8 +4,20 @@ import TransactionsItem from '../TransactionsItem/TransactionsItem';
 import s from './TransactionsList.module.css';
 // import { selectTransactions } from '../../redux/selectors';
 
+import 'overlayscrollbars/styles/overlayscrollbars.css';
+import {
+  OverlayScrollbars,
+  ScrollbarsHidingPlugin,
+  SizeObserverPlugin,
+  ClickScrollPlugin,
+} from 'overlayscrollbars';
+
+import { useEffect, useRef } from 'react';
+
 function TransactionsList() {
   //   const transactions = useSelector(selectTransactions);
+
+  const scrollRef = useRef(null);
 
   const transactions = [
     {
@@ -13,7 +25,7 @@ function TransactionsList() {
       date: '2025-05-01',
       type: 'income',
       category: 'Incomes',
-      comment: 'Salary for April and May',
+      comment: 'Salary for April ',
       sum: 2000,
     },
     {
@@ -93,7 +105,7 @@ function TransactionsList() {
       date: '2025-05-01',
       type: 'income',
       category: 'Incomes',
-      comment: 'Salary for April and May',
+      comment: 'Salary for April ',
       sum: 2000,
     },
     {
@@ -169,12 +181,34 @@ function TransactionsList() {
       sum: 25,
     },
   ];
-
   const isMobile = useMediaQuery({ query: '(max-width:768px)' });
+
+  useEffect(() => {
+    if (!isMobile && scrollRef.current) {
+      OverlayScrollbars.plugin([
+        ScrollbarsHidingPlugin,
+        SizeObserverPlugin,
+        ClickScrollPlugin,
+      ]);
+      const osInstance = OverlayScrollbars(scrollRef.current, {
+        scrollbars: {
+          // theme: 'os-theme-custom',
+          dragScroll: true,
+          clickScroll: true,
+          touchSupport: true,
+          theme: 'os-theme-light',
+        },
+      });
+
+      return () => {
+        if (osInstance) osInstance.destroy();
+      };
+    }
+  }, [isMobile]);
 
   return (
     <div className={s.wrapper}>
-      <div className={s.container}>
+      <div className={s.container} ref={scrollRef}>
         {transactions.length === 0 ? (
           <p className={s.stub}>There are no transactions yet</p>
         ) : (
--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,9 +1,9 @@
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 
-import { selectIsLoggedIn } from "../redux/auth/selectors";
+import { selectIsLoggedIn } from '../redux/auth/selectors';
 
-const PrivateRoute = ({ children, redirectTo = "/" }) => {
+const PrivateRoute = ({ children, redirectTo = '/' }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return isLoggedIn ? children : <Navigate to={redirectTo} />;
