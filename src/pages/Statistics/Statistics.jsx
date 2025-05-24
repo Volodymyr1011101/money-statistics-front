8bf3c68f11a565048bd9909125a84111a208b314
Volodymyr Voronkin <voronkinvladimir328@gmail.com>
2025-05-25 01:38:07 +0200

    added chart

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -2,13 +2,16 @@ import StatisticsDashboard from "../../components/UserAcountLayout/Statistics/St
 import css from "./Statistics.module.css";
 import FrontToggle from "../../components/UserAcountLayout/FrontToggle/FrontToggle";
 import StatisticsTable from "../../components/UserAcountLayout/StatisticsTable/StatisticsTable";
+import MyChart from "../../UI/Chart/Chart";
+import {useSelector} from "react-redux";
 
 const Statistics = () => {
+    const {balance} = useSelector(state => state.auth.user);
   return (
     <div className={css.statisticWrapper}>
       <div className={css.toggleChart}>
         <FrontToggle />
-        <div>Chart</div>
+          <MyChart balance={balance}/>
       </div>
       <div className={css.dashTable}>
         <StatisticsDashboard />
