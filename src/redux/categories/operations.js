92ba8fdcee61c289c99bf20e2dfb863bbd60ec07
Valeriia Martynova <vmartynova95@gmail.com>
2025-05-25 19:16:09 +0300

    update redux/categories/operation.js

--- src/redux/categories/operations.js
+++ src/redux/categories/operations.js
@@ -1,15 +1,15 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const api = axios.create({
-  baseURL: "https://money-statistics-back-1.onrender.com/catogories",
+export const categoriesApi = axios.create({
+  baseURL: "https://money-statistics-back-1.onrender.com/categories",
 });
 
 export const fetchCategories = createAsyncThunk(
   "categories/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get("/categories");
+      const response = await categoriesApi.get("/");
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
