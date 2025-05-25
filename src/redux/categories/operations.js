d92c475842660bb124c08518f1d601cfbe473b87
Valeriia Martynova <vmartynova95@gmail.com>
2025-05-25 21:12:49 +0300

    update redux/categories/operations.js

--- src/redux/categories/operations.js
+++ src/redux/categories/operations.js
@@ -5,14 +5,25 @@ export const categoriesApi = axios.create({
   baseURL: "https://money-statistics-back-1.onrender.com/categories",
 });
 
+const setAuthHeader = (token) => {
+  categoriesApi.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
 export const fetchCategories = createAsyncThunk(
   "categories/fetchAll",
-  async (_, thunkAPI) => {
+  async (_, { getState, rejectWithValue }) => {
     try {
+      const token = getState().auth?.token;
+      if (!token) {
+        return rejectWithValue("User is not authorized");
+      }
+
+      setAuthHeader(token);
+
       const response = await categoriesApi.get("/");
       return response.data;
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
+      return rejectWithValue(error.response?.data?.message || error.message);
     }
   }
 );
