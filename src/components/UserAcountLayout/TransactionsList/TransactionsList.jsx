77ad3cd99bdf6195b38d3df86b02e8289920f233
EugeneyCh <eugeney@ua.fm>
2025-05-26 12:52:50 +0300

    changeTransactionListToRealDataFromBackend

--- src/components/UserAcountLayout/TransactionsList/TransactionsList.jsx
+++ src/components/UserAcountLayout/TransactionsList/TransactionsList.jsx
@@ -1,10 +1,13 @@
-import TransactionsItem from '../TransactionsItem/TransactionsItem';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectTransactions } from '../../../redux/transaction/selectors';
+
 import s from './TransactionsList.module.css';
-import {useRef, useState} from 'react';
+
+import TransactionsItem from '../TransactionsItem/TransactionsItem';
 import ModalEditTransaction from '../../ModalEditTransaction/ModalEditTransaction';
 import ButtonAddTransaction from '../ButtonAddTransaction/ButtonAddTransaction';
-import ModalAddTransaction from "../../ModalAddTransaction/ModalAddTransaction";
-
+import ModalAddTransaction from '../../ModalAddTransaction/ModalAddTransaction';
 
 function TransactionsList() {
   const [showModalAddTransaction, setShowModalAddTransaction] = useState(false);
@@ -12,182 +15,18 @@ function TransactionsList() {
   const [showModalEditTransaction, setShowModalEditTransaction] =
     useState(false);
 
-  // const transactions = useSelector(selectTransactions);
+  const transactions = useSelector(
+    (state) => selectTransactions(state)?.result || []
+  );
 
-  const scrollRef = useRef(null);
-
-  //TODO: потрібно видалити після того як реалізується отримання даних Transactions від бекенду.
-  // наразі поля не співпадаютью
-  const transactions = [
-    {
-      id: '1',
-      date: '2025-05-01',
-      transactionType: 'income',
-      category: 'Incomes',
-      comment: 'Salary for April ',
-      sum: 2000,
-    },
-    {
-      id: '2',
-      date: '2025-05-02',
-      transactionType: 'expense',
-      category: 'Products',
-      comment: 'Groceries',
-      sum: 75,
-    },
-    {
-      id: '3',
-      date: '2025-05-03',
-      transactionType: 'expense',
-      category: 'Car',
-      comment: 'Gasoline',
-      sum: 50,
-    },
-    {
-      id: '4',
-      date: '2025-05-04',
-      transactionType: 'income',
-      category: 'Incomes',
-      comment: 'Freelance project',
-      sum: 500,
-    },
-    {
-      id: '5',
-      date: '2025-05-05',
-      transactionType: 'expense',
-      category: 'Leisure',
-      comment: 'Cinema',
-      sum: 20,
-    },
-    {
-      id: '6',
-      date: '2025-05-06',
-      transactionType: 'expense',
-      category: 'Education',
-      comment: 'Online course',
-      sum: 100,
-    },
-    {
-      id: '7',
-      date: '2025-05-07',
-      transactionType: 'expense',
-      category: 'Self care',
-      comment: 'Haircut',
-      sum: 30,
-    },
-    {
-      id: '8',
-      date: '2025-05-08',
-      transactionType: 'income',
-      category: 'Incomes',
-      comment: 'Bonus',
-      sum: 300,
-    },
-    {
-      id: '9',
-      date: '2025-05-09',
-      transactionType: 'expense',
-      category: 'Entertainment',
-      comment: 'Concert ticket',
-      sum: 60,
-    },
-    {
-      id: '10',
-      date: '2025-05-10',
-      transactionType: 'expense',
-      category: 'Household products',
-      comment: 'Cleaning supplies',
-      sum: 25,
-    },
-    {
-      id: '1',
-      date: '2025-05-01',
-      transactionType: 'income',
-      category: 'Incomes',
-      comment: 'Salary for April ',
-      sum: 2000,
-    },
-    {
-      id: '2',
-      date: '2025-05-02',
-      transactionType: 'expense',
-      category: 'Products',
-      comment: 'Groceries',
-      sum: 75,
-    },
-    {
-      id: '3',
-      date: '2025-05-03',
-      transactionType: 'expense',
-      category: 'Car',
-      comment: 'Gasoline',
-      sum: 50,
-    },
-    {
-      id: '4',
-      date: '2025-05-04',
-      transactionType: 'income',
-      category: 'Incomes',
-      comment: 'Freelance project',
-      sum: 500,
-    },
-    {
-      id: '5',
-      date: '2025-05-05',
-      transactionType: 'expense',
-      category: 'Leisure',
-      comment: 'Cinema',
-      sum: 20,
-    },
-    {
-      id: '6',
-      date: '2025-05-06',
-      transactionType: 'expense',
-      category: 'Education',
-      comment: 'Online course',
-      sum: 100,
-    },
-    {
-      id: '7',
-      date: '2025-05-07',
-      transactionType: 'expense',
-      category: 'Self care',
-      comment: 'Haircut',
-      sum: 30,
-    },
-    {
-      id: '8',
-      date: '2025-05-08',
-      transactionType: 'income',
-      category: 'Incomes',
-      comment: 'Bonus',
-      sum: 300,
-    },
-    {
-      id: '9',
-      date: '2025-05-09',
-      transactionType: 'expense',
-      category: 'Entertainment',
-      comment: 'Concert ticket',
-      sum: 60,
-    },
-    {
-      id: '10',
-      date: '2025-05-10',
-      transactionType: 'expense',
-      category: 'Household products',
-      comment: 'Cleaning supplies',
-      sum: 25,
-    },
-  ];
   const handleEditClick = (transaction) => {
     setSelectedTransaction(transaction);
     setShowModalEditTransaction(true);
   };
 
- const handleModalStateChange = () => {
-   setShowModalAddTransaction((prev) => !prev );
- }
+  const handleModalStateChange = () => {
+    setShowModalAddTransaction((prev) => !prev);
+  };
 
   return (
     <div className={s.wrapper}>
@@ -199,23 +38,15 @@ function TransactionsList() {
           <p>Comment</p>
           <p>Sum</p>
         </div>
-        <div
-          className={`${s.container} ${s[`scroll-container`]}`}
-          ref={scrollRef}
-        >
+        <div className={`${s.container} ${s[`scroll-container`]}`}>
           {transactions.length === 0 ? (
             <p className={s.stub}>There are no transactions yet</p>
           ) : (
             <>
               {transactions.map((item, index) => (
                 <TransactionsItem
-                  key={item.id + item.sum + index}
-                  id={item.id}
-                  date={item.date}
-                  type={item.transactionType}
-                  category={item.category}
-                  comment={item.comment}
-                  sum={item.sum}
+                  key={item.id}
+                  transaction={item}
                   onEdit={() => handleEditClick(item)}
                 />
               ))}
